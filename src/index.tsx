c8a163f7c6e5d728f22a5d4405e6ce94465f9404
Yan P Aung <yan.pye.aung@gmail.com>
2020-12-16 07:15:41 -0800

    Refactor state property using AppState generics and interface Note: use this approach or the previous apporach with property definition syntax. Do not mix or there will be bugs.

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,15 @@ interface AppProps {
   color?: string;
 }
 
-class App extends React.Component<AppProps> {
-  state = { counter: 0};
+interface AppState {
+  counter: number;
+}
+
+class App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = { counter: 0 };
+  }
 
   onIncrement = (): void => {
     this.setState({ counter: this.state.counter + 1 });
