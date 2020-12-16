be1513e09cf9632531306ae99add24d90762bc66
Yan P Aung <yan.pye.aung@gmail.com>
2020-12-16 07:22:32 -0800

    Comment out functional component and uncomment class based component

--- src/index.tsx
+++ src/index.tsx
@@ -6,41 +6,41 @@ interface AppProps {
 }
 
 // Functional Component example
-const App = (props: AppProps): JSX.Element => {
-  return <div>{props.color}</div>
-};
+// const App = (props: AppProps): JSX.Element => {
+//   return <div>{props.color}</div>
+// };
 
 
 // Class based component Example
 
-// interface AppState {
-//   counter: number;
-// }
-// 
-// class App extends React.Component<AppProps, AppState> {
-//   constructor(props: AppProps) {
-//     super(props);
-//     this.state = { counter: 0 };
-//   }
-// 
-//   onIncrement = (): void => {
-//     this.setState({ counter: this.state.counter + 1 });
-//   }
-// 
-//   onDecrement = (): void => {
-//     this.setState({ counter: this.state.counter - 1 });
-//   }
-// 
-//   render() {
-//     return (
-//       <div>
-//         <button onClick={this.onIncrement}>Increment</button>
-//         <button onClick={this.onDecrement}>Decrement</button>
-//         {this.state.counter}
-//       </div>
-//     );
-//   }
-// }
+interface AppState {
+  counter: number;
+}
+
+class App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = { counter: 0 };
+  }
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  }
+
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
   <App color="red" />,
