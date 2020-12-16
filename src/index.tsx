ce350402374abbb0f4d946e01229f0849eae22eb
Yan P Aung <yan.pye.aung@gmail.com>
2020-12-16 07:38:30 -0800

    Add boilerplate code for redux and react App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,48 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { App } from './components/App';
+import { reducers } from './reducers';
 
-interface AppProps {
-  color?: string;
-}
-
-// Functional Component example
-// const App = (props: AppProps): JSX.Element => {
-//   return <div>{props.color}</div>
-// };
-
-
-// Class based component Example
-
-interface AppState {
-  counter: number;
-}
-
-class App extends React.Component<AppProps, AppState> {
-  constructor(props: AppProps) {
-    super(props);
-    this.state = { counter: 0 };
-  }
-
-  onIncrement = (): void => {
-    this.setState({ counter: this.state.counter + 1 });
-  }
-
-  onDecrement = (): void => {
-    this.setState({ counter: this.state.counter - 1 });
-  }
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.onIncrement}>Increment</button>
-        <button onClick={this.onDecrement}>Decrement</button>
-        {this.state.counter}
-      </div>
-    );
-  }
-}
+const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <App color="red" />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.querySelector('#root')
 );
