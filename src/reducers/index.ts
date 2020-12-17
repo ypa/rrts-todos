baa6966556e9c3b5e2e27346406652e9aa388908
Yan P Aung <yan.pye.aung@gmail.com>
2020-12-17 07:28:16 -0800

    Add StoreState interface as combineReducers generics, which is a killer feature for using TS/Redux combo

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,6 +1,15 @@
 import { combineReducers } from 'redux';
 import { todosReducer } from './todos';
+import { Todo } from '../actions';
 
-export const reducers = combineReducers({
+// This is the interface of the entire state of Redux store.
+// It'll serve as documentation in addition to type safety
+// it brings with what reducers can return.
+// This is a killer feature of using TS with Redux.
+export interface StoreState {
+  todos: Todo[];
+}
+
+export const reducers = combineReducers<StoreState>({
   todos: todosReducer
 });
