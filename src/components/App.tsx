a9cf001aeccbf9c6ded447db050ab0459d3f11ae
Yan P Aung <yan.pye.aung@gmail.com>
2020-12-18 06:56:15 -0800

    Wire up App with connect -> mapStateToProps

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Todo, fetchTodos } from '../actions';
+import { StoreState } from '../reducers';
 
+interface AppProps {
+  todos: Todo[];
+  fetchTodos(): any;
+}
 
-export class App extends React.Component {
+class _App extends React.Component<AppProps> {
   render() {
     return <div>Hi there!</div>;
   }
 }
 
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+  return { todos: todos };
+};
+
+export const App = connect(
+  mapStateToProps,
+  {
+    fetchTodos: fetchTodos
+  }
+)(_App);
